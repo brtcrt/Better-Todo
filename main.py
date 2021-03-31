5abad320d98a01929d5e0b431c92bbe9e406a7a6
brt crt <bartuturan05@gmail.com>
2021-03-31 22:43:17 +0300

    Added alarms

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json  # Standard json library. Using this for data storage. Unstable but 
 from tkinter import ttk  # For quick access to parts like the Button.
 from tkcalendar import Calendar  # Used for the date picking.
 from datetime import date, datetime  # Used for setting starting date.
+import threading, time  # Threading for checking time.
 
 with open('tasks.json') as json_file:
     try:
@@ -23,12 +24,13 @@ def PickDate():  # To pick a date for the task.
     def print_sel():
         global addedTask, tasks_list, task
         AddTask()  # Adding the task from the input box. See the function for more info.
+        this_time = GetTime()  # Get time. See the function for more info.
         tasks = "tasks"  # This is just stupid but it fixed some error so I had to do it.
         if addedTask == "":  # Solves a problem where the program would die if the user enters an empty string. Idk who'd do that, but yeah. Just in case.
             messagebox.showinfo("Something bad happened :(", "Please enter your task first!")  # Also to keep everything in order. (Task first, date second)
         else:
             this_date = cal.selection_get()  # Gets the date.
-            task[tasks].append(str(addedTask) + " --- Task Date: " + str(this_date))  # Combines the task & date into a single string so it is easier to display.
+            task[tasks].append(str(addedTask) + " --- Task Date: " + str(this_date) + " --- " + str(this_time))  # Combines the task & date into a single string so it is easier to display.
             top.destroy()  # Auto close.
             SaveToJson()  # Saves to the json file. Also updates the list displayed. See the function for more info.
 
@@ -98,15 +100,15 @@ def DeleteTask():  # Delete Tasks.
             for member in remove_list:  # Remove the tasks.
                 task["tasks"].remove(member)
             SaveToJson()  # Save to json and update the list.
-            d_var.set("") # Reset the input box.
+            d_var.set("")  # Reset the input box.
             return
         except ValueError:
-            d_var.set("") # Reset the input box.
+            d_var.set("")  # Reset the input box.
             return messagebox.showinfo("Something bad happened :(", "Received bad characters!")  # If it isn't "all" or a number it would give an error so I'm just excepting that.
 
 
 def CheckDates():  # I spent so much time on this useless thing. There might be a faster and shorter way of doing it but I wrote it like this and can't be bothered to change it. But hey, it at least works.
-    global task    # And no, I'm not actually going to talk about this function in detail. Even I have trouble understanding what goes on.
+    global task  # And no, I'm not actually going to talk about this function in detail. Even I have trouble understanding what goes on.
     present = date.today()
     count = 0
     all_tasks = GetList()
@@ -119,18 +121,22 @@ def CheckDates():  # I spent so much time on this useless thing. There might be 
     past_tasks = []
     due_dates = []
     due_tasks = []
-    for i in range(1, len(all_tasks), 2):
-        all_dates.append(all_tasks[i])
+    for i in range(1, len(all_tasks), 3):
+        try:
+            all_dates.append(all_tasks[i])
+            print(all_tasks[i])
+        except IndexError:
+            print("Index Error")
     all_dates = "".join(all_dates).replace("Task Date: ", " ").split(" ")
     del all_dates[0]
     for dates in all_dates:
         current = dates.split("-")
-        current.insert(0, count + 1)
+        current.insert(0, str(count + 1))
         if date(int(current[1]), int(current[2]), int(current[3])) < present:
             past_dates.append(" ".join([str(elem) for elem in current]))
         if date(int(current[1]), int(current[2]), int(current[3])) == present:
             due_dates.append(" ".join([str(elem) for elem in current]))
-        count += 2
+        count += 3
     for elem in due_dates:
         cur = elem.split(" ")
         task_index.append(cur[0])
@@ -161,6 +167,39 @@ def ShowAtStart():
         return messagebox.showinfo("Attention", total)
 
 
+def GetTime():
+    global minute_var, hour_var
+    return hour_var.get() + ":" + minute_var.get()
+
+
+def CheckTime():
+    global minutes, task
+    time_up = []
+    while True:
+        if str(datetime.now().minute) in minutes:
+            current_items = GetList().split("\n")
+            del current_items[len(current_items) - 1]
+            for item in current_items:
+                new_item = item.split(" --- ")
+                print(new_item)
+                print("Task Date: {}".format(str(date.today())))
+                if new_item[1] != "Task Date: {}".format(str(date.today())):
+                    return
+                date_item = new_item[2].split(":")
+                print(date_item)
+                if date_item[0] == str(datetime.now().hour) and date_item[1] == str(datetime.now().minute):
+                    time_up.append(new_item[0])
+            if time_up == []:
+                time.sleep(60)
+                pass
+            else:
+                latests_alarms = "\n".join(time_up)
+                messagebox.showinfo("Time is up!", latests_alarms)
+                time.sleep(60)
+                return "Done!"
+        time.sleep(60)
+
+
 root = tk.Tk()  # The core of the tkinter interface system.
 s = ttk.Style(root)  # Some styling.
 s.theme_use('clam')  # Some more styling.
@@ -168,6 +207,18 @@ t_var = tk.StringVar()  # String variables to reset it back to "".
 d_var = tk.StringVar()  # ^^^^^^^^
 task_entry = ttk.Entry(root, textvariable=t_var)  # Getting the input from the user. This is the thing which adds the little input box.
 task_entry.pack(padx=10, pady=10, fill="both", expand=1)  # Packing it separately because it wouldn't stop giving errors.
+hours = ["01", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00"]
+hour_var = tk.StringVar()
+hour_var.set("12")
+minutes = ["00", "00", "10", "15", "20", "30", "40", "45", "50"]
+minute_var = tk.StringVar()
+minute_var.set("00")
+hour_label = ttk.Label(root, text="Hour: ").pack(padx=10, pady=10)
+hour_select = ttk.OptionMenu(root, hour_var, *hours)
+hour_select.pack(padx=10, pady=10)
+minute_label = ttk.Label(root, text="Minute: ").pack(padx=10, pady=10)
+minute_select = ttk.OptionMenu(root, minute_var, *minutes)
+minute_select.pack(padx=10, pady=10)
 ttk.Button(root, text="Set task", command=PickDate).pack(padx=10, pady=10)  # Button for setting task.
 delete_entry = ttk.Entry(root, textvariable=d_var)  # Delete input box.
 delete_entry.pack(padx=10, pady=10, fill="both")  # Pack.
@@ -185,4 +236,6 @@ root.geometry("{}x{}+{}+{}".format(w_width, w_height, s_left, s_right))  # Cente
 root.title("Todo List")  # Title
 root.iconbitmap("icon.ico")  # Icon
 ShowAtStart()  # Call this function at start-up to see if there is anything relevant.
+thread = threading.Thread(target=CheckTime)
+thread.start()
 root.mainloop()  # Finally initialize the program.
